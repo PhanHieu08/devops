21e383aaeb6befb1c829370f07068db7e62166c7
Hieu Phan <phanhieu.hust64@gmail.com>
2023-04-02 22:12:10 +0700

    Add Lab 4: Write Bash script

--- Linux/Buoi_3/Lab_4.sh
+++ Linux/Buoi_3/Lab_4.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
-env
-printenv SHELL
-export MY_VAR="hello world"
-echo $MY_VAR
-unset MY_VAR
-echo $MY_VAR
-echo 'export NEW_VAR="new persistent varialbe"' >> ~/.bashrc
-source ~/.bashrc
-echo $NEW_VAR
+touch ~/myscript.sh
+echo "#!/bin/bash" > ~/myscript.sh
+echo "echo 'hello, world!'" >> ~/myscript.sh
+ls -l
+chmod +x ~/myscript.sh
+~/myscript.sh
+
